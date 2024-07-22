5b6529afd0925e96c93d36e156424b0e6179ff62
troshin.matvey@yandex.ru <108873914+onodaHiroo@users.noreply.github.com>
2024-07-22 21:27:44 +0300

    init

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,120 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class UserDaoJDBCImpl implements UserDao {
+public class UserDaoJDBCImpl extends Util implements UserDao {
+
+    private static final Logger LOGGER = Logger.getLogger(UserDaoJDBCImpl.class.getName());
+    private static final Connection connection = Util.getConnection();
+
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
+        String sqlQuery = new StringBuilder("CREATE TABLE IF NOT EXISTS users (").
+                append("id BIGINT NOT NULL AUTO_INCREMENT, ")
+                .append("name VARCHAR(45) NULL, ")
+                .append("lastname VARCHAR(45) NULL, ")
+                .append("age TINYINT NULL, ")
+                .append("PRIMARY KEY (id), ")
+                .append("UNIQUE INDEX id_UNIQUE (id ASC) VISIBLE)")
+                .toString();
 
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(sqlQuery);
+            LOGGER.log(Level.INFO, "Таблица создана");
+        } catch (SQLException e) {
+            LOGGER.log(Level.SEVERE, "При создании таблицы произошла ошибка");
+        }
     }
 
     public void dropUsersTable() {
+        String sqlQuery = "DROP TABLE IF EXISTS users";
+
+        try (Statement statement = connection.createStatement()) {
+
+            statement.executeUpdate(sqlQuery);
+            LOGGER.log(Level.INFO, "Таблица была удаленна");
+        } catch (SQLException e) {
+            LOGGER.log(Level.SEVERE, "При удалении таблицы произошла ошибка");
+        }
 
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        String sqlQuery = "INSERT INTO users (name, lastname, age) VALUES (?, ?, ?)";
 
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery)) {
+
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
+            preparedStatement.executeUpdate();
+
+            LOGGER.log(Level.INFO, "В таблицу добавлена новая запись");
+        } catch (SQLException e) {
+            LOGGER.log(Level.SEVERE, "При добавлении записи произошла ошибка");
+        }
     }
 
     public void removeUserById(long id) {
+        String sqlQuery = "DELETE FROM users WHERE id = ?";
 
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery)) {
+
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
+
+            LOGGER.log(Level.INFO, "В таблице удалена запись с индексом " + id);
+        } catch (SQLException e) {
+            LOGGER.log(Level.SEVERE, "При удалении записи с индексом " + id + " произошла ошибка");
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        String sqlQuery = "SELECT * FROM users";
+        List<User> list = new ArrayList<>();
+
+        try (
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(sqlQuery)) {
+
+            while (resultSet.next()) {
+                User user = new User();
+
+                user.setId(resultSet.getLong(1));
+                user.setName(resultSet.getString(2));
+                user.setLastName(resultSet.getString(3));
+                user.setAge(resultSet.getByte(4));
+
+                list.add(user);
+            }
+            LOGGER.log(Level.INFO, "Из таблицы успешно получены все данные");
+            System.out.println(list);
+            return list;
+        } catch (SQLException e) {
+            LOGGER.log(Level.SEVERE, "При получении всех данных из таблицы произошла ошибка");
+            return null;
+        }
     }
 
     public void cleanUsersTable() {
+        String sqlQuery = "TRUNCATE TABLE users";
 
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(sqlQuery);
+            LOGGER.log(Level.INFO, "Таблица очищена");
+        } catch (SQLException e) {
+            LOGGER.log(Level.SEVERE, "Таблица не очищена");
+        }
     }
+
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,27 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/pp_1_1_3-4_jdbc_hibernate";
+    private static final String USER = "root";
+    private static final String PASSWORD = "rootlocalhost123";
+    private static final Logger LOGGER = Logger.getLogger(Util.class.getName());
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            Class.forName(DRIVER);
+            LOGGER.log(Level.FINE, "Подключение к базе данных произошло успешно.");
+        } catch (ClassNotFoundException | SQLException ex) {
+            LOGGER.log(Level.SEVERE, "При подключении к базе данных произошла ошибка.", ex);
+        }
+    return connection;
+    }
 }
